91d96698858fa9e674f898cf8a2f37b21dab0a7a
Shir Rubin <shir.rubin01@runi.ac.il>
2023-12-25 18:56:35 +0200

    Damka correctiom

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,13 +5,13 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
 		for (int i = 0; i < n; i++) {
-			System.out.println("");
 			if (i%2!=0) {
 				System.out.print(" ");
 			}
 			for (int j = 0; j < n; j++) {
 				System.out.print("* ");
 			}
+			System.out.println("");
 		}
 	}
 }
